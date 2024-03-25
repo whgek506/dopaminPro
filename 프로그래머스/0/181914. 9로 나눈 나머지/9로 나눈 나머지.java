df8fa76d2de9e9297b835b1bf7aecba92d02d5a3
whgek506 <154417844+whgek506@users.noreply.github.com>
2024-03-25 23:31:35 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.05 ms, Memory: 92.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,13 @@
+import java.lang.Integer;
+
+class Solution {
+    public int solution(String number) {
+        int sum = 0;
+        for (int i = 0; i < number.length(); i++) {
+            sum += Character.getNumericValue(number.charAt(i));
+        }
+        
+        int remainder = sum % 9;
+        return remainder;
+    }
+}
